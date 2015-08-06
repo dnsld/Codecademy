ddfdeccb847b680200261417da89568aeaa6f3c8
d5al57 <dansaldana@gmail.com>
2015-08-06 09:42:41 -0400

    git test

--- Javascript/coin_flipping.js
+++ Javascript/coin_flipping.js
@@ -10,3 +10,4 @@ console.log("Tails! Done flipping.");
 // while the coin came up heads (when coinFace equalled 0), 
 // it would flip again, and it would stop flipping once it got tails 
 //(when coinFace was 1). Since the flip was random, we didn't know ahead of time how many loops we'd need.
+/// git test
